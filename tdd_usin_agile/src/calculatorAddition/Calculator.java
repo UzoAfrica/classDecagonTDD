3cf6dd6994dccd30119da6a89bd4a3fbc680ab0b
UzoAfrica <ibezimuzomajoseph@gmail.com>
2023-03-28 22:39:59 +0100

    Assignment on word Count.

--- tdd_usin_agile/src/calculatorAddition/Calculator.java
+++ tdd_usin_agile/src/calculatorAddition/Calculator.java
@@ -2,6 +2,21 @@ package calculatorAddition;
 
 public class Calculator {
     public int calculate (int firstNumber, int secondNumber, String operator) {
-        return null;
+        int result = 0;
+        switch(operator){
+            case "+":
+                result= firstNumber + secondNumber;
+                break;
+            case "-":
+                result= firstNumber - secondNumber;
+                break;
+            case "*":
+                result= firstNumber * secondNumber;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+
+        return result;
     }
 }
--- tdd_usin_agile/src/wordCounterExercise/WordCounterTest.java
+++ tdd_usin_agile/src/wordCounterExercise/WordCounterTest.java
@@ -1,2 +1,39 @@
-package wordCounterExercise;public class WordCounterTest {
+package wordCounterExercise;
+
+import calculatorAddition.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WordCounterTest {
+
+    @Test
+    public void return0IfInputIsEmpty() {
+        WordCounter wordCounter = new WordCounter();
+        String input = "";
+        int actualOutput = wordCounter.wordCount(input);
+        assertEquals(0, actualOutput);
+    }
+
+    @Test
+    public void return1IfInputContainSingleWord(){
+        WordCounter wordCounter= new WordCounter();
+        String input = "One";
+        int actualOutput = wordCounter.wordCount(input);
+        assertEquals(1, actualOutput);
+    }
+    @Test
+    public void returnTheNumberOfWords(){
+        WordCounter wordCounter= new WordCounter();
+        String input = "One Two Three";
+        int actualOutput = wordCounter.wordCount(input);
+        assertEquals(3, actualOutput);
+    }
+    @Test
+    public void returnTheNumberOfMultipleWords(){
+        WordCounter wordCounter= new WordCounter();
+        String input = "One Two Three, Four, Five";
+        int actualOutput = wordCounter.wordCount(input);
+        assertEquals(5, actualOutput);
+    }
 }
